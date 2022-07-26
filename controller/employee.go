c6c4cfe054d797353b5496c285246a07a4d406d0
neha-rose <neharosegeorge@gmail.com>
2022-07-26 15:42:33 +0530

    created api to upload id proof

--- controller/employee.go
+++ controller/employee.go
@@ -1,11 +1,15 @@
 package controller
 
 import (
+	"net/http"
+	"path/filepath"
+
 	"github.com/Kemosabe2911/employee-app-go/dto"
 	"github.com/Kemosabe2911/employee-app-go/helpers"
 	"github.com/Kemosabe2911/employee-app-go/logger"
 	"github.com/Kemosabe2911/employee-app-go/service"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type EmployeeController struct {
@@ -59,4 +63,35 @@ func (ec *EmployeeController) UpdateEmployee(c *gin.Context) {
 	resp := ec.EmployeeService.UpdateEmployee(id, employeeData)
 	c.JSON(resp.StatusCode, resp.Data)
 	logger.Info("End UpdateEmployee - Controller")
+}
+
+func (ec *EmployeeController) UploadIdProof(c *gin.Context) {
+	logger.Info("Start UploadIdProof in Controller")
+	id := c.Param("id")
+	file, err := c.FormFile("file")
+
+	logger.Info(file.Filename)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "No file is received",
+		})
+		return
+	}
+
+	extension := filepath.Ext(file.Filename)
+	// Generate random file name for the new uploaded file so it doesn't override the old file with same name
+	newFileName := uuid.New().String() + extension
+
+	if err := c.SaveUploadedFile(file, "./assets/"+newFileName); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Unable to save the file",
+		})
+		logger.Info(err)
+		return
+	}
+
+	resp := ec.EmployeeService.UploadIdProof(id, newFileName)
+	c.JSON(resp.StatusCode, resp.Data)
+	logger.Info("End UploadIdProof in Controller")
 }
